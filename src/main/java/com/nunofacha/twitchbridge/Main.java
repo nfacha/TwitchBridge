6df56fb2330634ad6e843a4678561eb43fbfe233
Nuno Facha <nunofacha@gmail.com>
2021-03-27 16:04:18 -0100

    Update checker and metrics

--- src/main/java/com/nunofacha/twitchbridge/Main.java
+++ src/main/java/com/nunofacha/twitchbridge/Main.java
@@ -1,10 +1,13 @@
 package com.nunofacha.twitchbridge;
 
 import com.nunofacha.twitchbridge.libs.FileManager;
+import com.nunofacha.twitchbridge.updater.Updater;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,11 +26,27 @@ public final class Main extends JavaPlugin {
         fileManager.getConfig("config.yml").copyDefaults(true).save();
         Main.pluginConfig = fileManager.getConfig("config.yml").get();
         logger.info("Config loaded");
+        updateCheck();
+        Metrics metrics = new Metrics(this, 10824);
         TwitchChat.init();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    private void updateCheck() {
+        try {
+
+            if (getConfig().getBoolean("auto-update", true)) {
+                Main.logger.info("Using the " + getConfig().getString("update-channel") + " update channel!");
+                Updater updater = new Updater("https://raw.githubusercontent.com/nfacha/TwitchBridge/" + getConfig().getString("update-channel") + "/meta.json");
+            } else {
+                Main.logger.info("Auto updating is disabled!");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
