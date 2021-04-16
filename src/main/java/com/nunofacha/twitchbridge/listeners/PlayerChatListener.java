e9f52969b2196a94bcdf8af22e97795cf2c5ef73
Nuno Facha <nunofacha@gmail.com>
2021-04-16 15:35:45 +0000

    Implement streamer only mode

--- src/main/java/com/nunofacha/twitchbridge/listeners/PlayerChatListener.java
+++ src/main/java/com/nunofacha/twitchbridge/listeners/PlayerChatListener.java
@@ -1,5 +1,6 @@
 package com.nunofacha.twitchbridge.listeners;
 
+import com.nunofacha.twitchbridge.Main;
 import com.nunofacha.twitchbridge.TwitchChat;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
@@ -11,6 +12,9 @@ public class PlayerChatListener implements Listener {
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
         if (e.getPlayer().hasPermission("twitch.chat.send") && e.getMessage().startsWith(":")) {
+            if(Main.pluginConfig.getBoolean("streamer-only") && !e.getPlayer().getName().equalsIgnoreCase(Main.pluginConfig.getString("streamer-name"))){
+                return;
+            }
             e.setCancelled(true);
             String msg = e.getMessage().substring(1);
             TwitchChat.sendMessage(msg);
